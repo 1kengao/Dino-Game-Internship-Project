f1ec71aaddfbea71b356b050f05cb5a79dc93d4c
kg-sato <kg.sato.main@gmail.com>
2026-05-27 10:47:17 -0400

    Added Custom Font For Program. Removed Background for Score. Added Level UP (Temporary, use will finish after tutorial is finished) Added Subtitle for Menu Removed Bug where score was accumilating while in game over Menu

--- main.py
+++ main.py
@@ -23,14 +23,17 @@ JUMP_GRAVITY_START_SPEED = -20  # The speed at which the player jumps
 players_gravity_speed = 0  # The current speed at which the player falls
 start_time = pygame.time.get_ticks() # returns time since start
 score = 0 # initializes the score
+instance_score = 0 # Added to Remove bug in game where Score changes while still in menu
 high_score = 0 
 
+# Load Font
+game_font = pygame.font.Font("graphics/font/Minecraft.ttf", 48)
+small_font = pygame.font.Font("graphics/font/Minecraft.ttf", 24)
+
 # Load level assets
-SKY_SURF = pygame.image.load("graphics/level/sky.png").convert()
-GROUND_SURF = pygame.image.load("graphics/level/ground.png").convert()
-game_font = pygame.font.Font(pygame.font.get_default_font(), 50)
-small_font = pygame.font.Font(pygame.font.get_default_font(), 24)
-score_surf = game_font.render("SCORE?", False, "Black")
+SKY_SURF = pygame.image.load("graphics/level/sky.png").convert_alpha()
+GROUND_SURF = pygame.image.load("graphics/level/ground.png").convert_alpha()
+score_surf = game_font.render("0", False, "Black")
 score_rect = score_surf.get_rect(center=(400, 50))
 
 # Load sprite assets
@@ -38,11 +41,15 @@ player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alp
 player_rect = player_surf.get_rect(bottomleft=(25, GROUND_Y))
 egg_surf = pygame.image.load("graphics/egg/egg_1.png").convert_alpha()
 egg_rect = egg_surf.get_rect(bottomleft=(800, GROUND_Y))
+level_up_surf = pygame.image.load("graphics/player/Level Up.png").convert_alpha()
+level_up_rect = level_up_surf.get_rect(center=(400, 200))
 
 #Load Menu Assets
 title_surf = pygame.image.load("graphics/level/dino_game.png").convert_alpha()
 title_surf = pygame.transform.scale_by(title_surf, 4)
-title_rect = title_surf.get_rect(center=(400, 130))
+title_rect = title_surf.get_rect(center=(400, 115))
+sub_title_surf = game_font.render("Survive Extinction", False, "Black")
+sub_title_rect = sub_title_surf.get_rect(center=(400, 180))
 press_space_surf = pygame.image.load("graphics/level/press_space.png").convert_alpha()
 press_space_surf = pygame.transform.scale_by(press_space_surf, 4)
 prompt_rect = press_space_surf.get_rect(center=(400, 240))
@@ -50,11 +57,9 @@ retry_rect = press_space_surf.get_rect(center=(400, 260))
 game_over_surf = game_font.render("GAME OVER", False, "Black")
 game_over_rect = game_over_surf.get_rect(center=(400, 130))
 
-
-
 while running:
     # Gets the score based off time survived and renders it center top
-    score = (pygame.time.get_ticks() - start_time) // 10
+    score = (pygame.time.get_ticks() - start_time) // 100
     score_surf = game_font.render(str(score), False, "Black")
     score_rect = score_surf.get_rect(center=(400, 50))
     # Poll for events
@@ -62,7 +67,6 @@ while running:
         # pygame.QUIT --> user clicked X to close your window
         if event.type == pygame.QUIT:
             running = False
-            continue
 
         if game_state == STATE_PLAYING:
             # When player wants to jump by pressing SPACE
@@ -79,34 +83,38 @@ while running:
                 player_rect.bottomleft = (25, GROUND_Y)
                 start_time = pygame.time.get_ticks() # Restarts Timer
                 game_state = STATE_PLAYING
+
     if game_state == STATE_PLAYING:
         screen.fill("purple")  # Wipe the screen
 
         # Blit the level assets
         screen.blit(SKY_SURF, (0, 0))
         screen.blit(GROUND_SURF, (0, GROUND_Y))
-        pygame.draw.rect(screen, "#c0e8ec", score_rect)
-        pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
         screen.blit(score_surf, score_rect)
 
         # Adjust egg's horizontal location then blit it
-        egg_rect.x -= 5
+        egg_rect.x -= 8
         if egg_rect.right <= 0:
             egg_rect.left = 800
         screen.blit(egg_surf, egg_rect)
 
         # Adjust player's vertical location then blit it
-        players_gravity_speed += 1
+        players_gravity_speed += 1.75
         player_rect.y += players_gravity_speed
         if player_rect.bottom > GROUND_Y:
             player_rect.bottom = GROUND_Y
         screen.blit(player_surf, player_rect)
 
+        # Levels 'UP' the player once it reaches a threshold. Changing soon as I add projectiles and more stuff
+        if score % 25 == 0 and score != 0:
+            screen.blit(level_up_surf, level_up_rect)
+            
         # When player collides with enemy, game ends
         if egg_rect.colliderect(player_rect):
             if score > high_score:
                 high_score = score
             game_state = STATE_GAME_OVER
+            instance_score = score
     
     # If game just initialized, shows menu.
     elif game_state == STATE_MENU:
@@ -115,6 +123,7 @@ while running:
         screen.blit(player_surf, player_rect)
         screen.blit(title_surf, title_rect)
         screen.blit(press_space_surf, prompt_rect)
+        screen.blit(sub_title_surf, sub_title_rect)
     
     # When game is over, display game over message
     else:
@@ -122,7 +131,7 @@ while running:
         screen.blit(GROUND_SURF, (0, GROUND_Y))
         screen.blit(game_over_surf, game_over_rect)
         final_score_surf = small_font.render(
-            f"Score: {score}    Best: {high_score}", False, "Black"
+            f"Your Score: {instance_score}   Best: {high_score}", False, "Black"
         )
         final_score_rect = final_score_surf.get_rect(center=(400, 200))
         screen.blit(final_score_surf, final_score_rect)
